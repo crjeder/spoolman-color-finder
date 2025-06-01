f6457079cbe948fa109c92c86e5d5ec9329ae148
Christoph <crjeder@gmail.com>
2025-06-01 17:09:48 +0200

    korrektur

--- color.py
+++ color.py
@@ -11,7 +11,8 @@ parser = argparse.ArgumentParser(description="Finds filaments in spoolman which 
                                  prog="Spoolman-color-finder")
 parser.add_argument("color", type=str, help="the color value in hex to search for")
 parser.add_argument("--url", "-u", type=str, default="http://127.0.0.1")
-parser.add_argument("-d", "--distance", type=int, default = 15, help="how different the colors are allowed to be. (high value means differs a lot)")
+parser.add_argument("-d", "--distance", type=int, default = 15, 
+                    help="how different the colors are allowed to be. (high value means differs a lot)")
 
 args = parser.parse_args()
 url = args.url + "/api/v1/export/filaments"
@@ -24,7 +25,7 @@ match_color_srgb = sRGBColor(int(hex_color[0:2], 16),
                             int(hex_color[4:6], 16), True)
 
 match_color_lab = convert_color(match_color_srgb, LabColor)
-list = []
+color_list = []
 params = {"fmt": "json"}
 
 try:
@@ -48,7 +49,7 @@ if response.status_code == 200:
     data = response.json()
     for row in data:
         hex_color = row["color_hex"]
-        if hex_color != None:
+        if hex_color is not None:
             hex_color = hex_color.lstrip('#')  # Remove '#' if present
             rgb = sRGBColor(int(hex_color[0:2], 16),
                             int(hex_color[2:4], 16),
@@ -60,13 +61,13 @@ if response.status_code == 200:
 
             if distance < max_distance:
                 match = (row["id"], row["name"], distance)
-                list.append(match)
+                color_list.append(match)
 else:
     print("Fehler:", response.status_code)
     print(response.json)
 
-if len(list) > 0:
-    sorted_list = sorted(list, key=lambda x: x[2])
+if len(color_list) > 0:
+    sorted_list = sorted(color_list, key=lambda x: x[2])
     for item in sorted_list:
         print(item)
 else:
