4c9f764bf458f95a20aea0bbe38d410b2cf3a5d3
Christoph <crjeder@gmail.com>
2025-06-01 13:56:37 +0200

    new

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# spoolman-color-finder
+
+This is just a little tool for the [spoolman](https://github.com/Donkie/Spoolman) API. It finds filaments with a color close enough to the one you specified.
+
+# usage
+Spoolman-color-finder [-h] [--url URL] [-d DISTANCE] color
+
+
+positional arguments:
+  color                 the color value in hex to search for
+
+options:
+  -h, --help            show this help message and exit
+  --url URL, -u URL
+  -d DISTANCE, --distance DISTANCE
+                        how different the colors are allowed to be. (high value means differs a lot)
+
--- color.py
+++ color.py
@@ -1,27 +1,47 @@
 import math
+import sys
 from colormath.color_objects import sRGBColor, LabColor
 from colormath.color_conversions import convert_color
 import requests
 import json
 import argparse
 
-# parameter defaults
-url = "http://127.0.0.1" # API-base url
-match_color_srgb = sRGBColor(255, 144, 0, True)
-max_distance = 1200
-
 # Create argument parser
 parser = argparse.ArgumentParser(description="Finds filaments in spoolman which match the color you specified.",
                                  prog="Spoolman-color-finder")
-parser.add_argument("--api-url", "-u", type=str, description="URL of the Spoolman API (whitout '/api/v1/')")  # Optional argument
-parser.add_argument("--color-hex", "-c", type=str, description="the color value in hex to search for")
-parser.add_argument("-d", "--distance", type=int, description="how different the colors are allowed to be. (high value means differs a lot)")
-url.append("/api/v1/export/filaments")
+parser.add_argument("color", type=str, help="the color value in hex to search for")
+parser.add_argument("--url", "-u", type=str, default="http://127.0.0.1")
+parser.add_argument("-d", "--distance", type=int, default = 15, help="how different the colors are allowed to be. (high value means differs a lot)")
+
+args = parser.parse_args()
+url = args.url + "/api/v1/export/filaments"
+max_distance = args.distance
+hex_color = args.color
+hex_color = hex_color.rstrip("#")
+
+match_color_srgb = sRGBColor(int(hex_color[0:2], 16),
+                            int(hex_color[2:4], 16),
+                            int(hex_color[4:6], 16), True)
 
 match_color_lab = convert_color(match_color_srgb, LabColor)
 list = []
 params = {"fmt": "json"}
-response = requests.get(url, params=params)
+
+try:
+    response = requests.get(url, params=params)
+    response.raise_for_status()  # Raises HTTPError if the response contains an error (4xx or 5xx)
+except requests.exceptions.HTTPError as errh:
+    print("HTTP Error:", errh)
+    sys.exit(errh)
+except requests.exceptions.ConnectionError as errc:
+    print("Connection Error: could not connect to ", url)
+    sys.exit(errc)
+except requests.exceptions.Timeout as errt:
+    print("Timeout Error:", errt)
+    sys.exit(errt)
+except requests.exceptions.RequestException as err:
+    print("Something went wrong:", err)
+    sys.exit(err)
 
 # Antwort prüfen und ausgeben
 if response.status_code == 200:
@@ -34,18 +54,21 @@ if response.status_code == 200:
                             int(hex_color[2:4], 16),
                             int(hex_color[4:6], 16), True)
             lab = convert_color(rgb, LabColor)
-            square_distance = ((lab.lab_a - match_color_lab.lab_a)**2 +
+            distance = math.sqrt((lab.lab_a - match_color_lab.lab_a)**2 +
                                (lab.lab_b - match_color_lab.lab_b)**2 +
                                (lab.lab_l - match_color_lab.lab_l)**2)
 
-            if square_distance < max_distance:
-                match = (row["id"], row["name"], square_distance)
+            if distance < max_distance:
+                match = (row["id"], row["name"], distance)
                 list.append(match)
 else:
     print("Fehler:", response.status_code)
     print(response.json)
 
-sorted_list = sorted(list, key=lambda x: x[2])
-for id in sorted_list:
-    print(id, name, distance)
+if len(list) > 0:
+    sorted_list = sorted(list, key=lambda x: x[2])
+    for item in sorted_list:
+        print(item)
+else:
+    print("No filaments matching your color found")
 
